5572de4d5656907ecda29e9a016aa63e2ddf89b8
susmita-subedi <sushmita.subedi@gmail.com>
2017-09-21 08:42:33 -0700

    adding activities for single car and fleet with intents working

---
+++ app/src/main/java/com/android/susmita/ecalcharge_mercedez_android/Fleet.java
@@ -0,0 +1,18 @@
+package com.android.susmita.ecalcharge_mercedez_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by susmita on 9/20/2017.
+ */
+
+public class Fleet extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fleet);
+    }
+}
